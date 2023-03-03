8374f6a77b4010c039a51f2846f958f5eb51059e
Maxime GAGLIARDINI <ext.maxime.gagliardini@sncf.fr>
2023-03-03 17:50:31 +0100

    test

--- resources/update_keystore.sh
+++ resources/update_keystore.sh
@@ -2,8 +2,7 @@
 
 CACERTS_PATH=$(find /usr /opt -name cacerts | grep $JAVA_HOME)
 
-if [ -z "$CACERTS_PATH"  ]
+if [ ! -z "$CACERTS_PATH"  ]
 then
-    RUN yes y | keytool -importcert -keystore $CACERTS_PATH -storepass changeit -file /usr/local/share/ca-certificates/ca-racine-sncf.crt
-EOF
+    yes y | keytool -importcert -keystore $CACERTS_PATH -storepass changeit -file /usr/local/share/ca-certificates/ca-racine-sncf.crt
 fi
