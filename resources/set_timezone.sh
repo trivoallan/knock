ccb7d000dcc4412708cee14db94455a4052044c9
Maxime GAGLIARDINI <ext.maxime.gagliardini@sncf.fr>
2023-05-10 15:20:50 +0200

    test

---
+++ resources/set_timezone.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. /etc/os-release
+
+if [ $ID = "alpine"  ]
+then
+    apk add --no-cache tzdata
+else
+    apt-get update
+    apt-get install -yq tzdata
+    ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+    dpkg-reconfigure -f noninteractive tzdata
+    rm -rf /var/lib/apt/lists/*
+fi
