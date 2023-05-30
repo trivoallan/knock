9f82028ec3de74264e45a3e9000c69f67c03f5ab
Maxime GAGLIARDINI <ext.maxime.gagliardini@sncf.fr>
2023-05-30 09:33:25 +0200

    Test

--- resources/add_sncf_http_sources.sh
+++ resources/add_sncf_http_sources.sh
@@ -5,18 +5,18 @@
 if [ $ID = "debian"  ] && [ $VERSION_CODENAME = "bullseye" ]
 then
     cat <<EOF > /etc/apt/sources.list
-deb https://repos.it.sncf.fr/debian bullseye main non-free contrib
-deb https://repos.it.sncf.fr/debian bullseye-updates main non-free contrib
-deb https://repos.it.sncf.fr/debian-security bullseye-security main non-free contrib
+deb http://repos.it.sncf.fr/debian bullseye main non-free contrib
+deb http://repos.it.sncf.fr/debian bullseye-updates main non-free contrib
+deb http://repos.it.sncf.fr/debian-security bullseye-security main non-free contrib
 EOF
 fi
 
 if [ $ID = "debian"  ] && [ $VERSION_CODENAME != "bullseye" ]
 then
     cat <<EOF > /etc/apt/sources.list
-deb https://repos.it.sncf.fr/debian ${VERSION_CODENAME} main non-free contrib
-deb https://repos.it.sncf.fr/debian ${VERSION_CODENAME}-updates main non-free contrib
-deb https://repos.it.sncf.fr/debian-security ${VERSION_CODENAME}/updates main non-free contrib
+deb http://repos.it.sncf.fr/debian ${VERSION_CODENAME} main non-free contrib
+deb http://repos.it.sncf.fr/debian ${VERSION_CODENAME}-updates main non-free contrib
+deb http://repos.it.sncf.fr/debian-security ${VERSION_CODENAME}/updates main non-free contrib
 EOF
 fi
 
