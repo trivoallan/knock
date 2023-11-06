c777f51e2c518ad6f941dacf4a9374fc03dae3b1
PMGI01232 <ext.maxime.gagliardini@sncf.fr>
2023-11-06 14:03:04 +0100

    Ajout AC_INFRASTRUCTURE_SNCF

--- resources/update_keystore.sh
+++ resources/update_keystore.sh
@@ -8,9 +8,10 @@ if [ ! -z "$CACERTS_PATH" ]
 then
     if [ ! -z "$DEBIAN" ]
     then
-        yes y | keytool -importcert -alias AC_RACINE_SNCF -keystore $CACERTS_PATH -storepass changeit -file /usr/local/share/ca-certificates/ca-racine-sncf.crt || true
         yes y | keytool -importcert -alias AC_RACINE_SNCF_2023 -keystore $CACERTS_PATH -storepass changeit -file /usr/local/share/ca-certificates/AC_RACINE_SNCF_2023.crt || true
         yes y | keytool -importcert -alias AC_INFRASTRUCTURE_SNCF_2023 -keystore $CACERTS_PATH -storepass changeit -file /usr/local/share/ca-certificates/AC_INFRASTRUCTURE_SNCF_2023.crt || true
+        yes y | keytool -importcert -alias AC_RACINE_SNCF -keystore $CACERTS_PATH -storepass changeit -file /usr/local/share/ca-certificates/ca-racine-sncf.crt || true
+        yes y | keytool -importcert -alias AC_INFRASTRUCTURE_SNCF -keystore $CACERTS_PATH -storepass changeit -file /usr/local/share/ca-certificates/AC_INFRASTRUCTURE_SNCF.crt || true
     fi
     if [ ! -z "$CENTOS" ]
     then
